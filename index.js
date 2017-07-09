8f4a61da25154d03ef82164f9fc42a14d5f8facc
Tobias Davis <accounts@davistobias.com>
2017-07-09 01:26:40 -0500

    use tabs for indentation

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ delete options._
 
 const get = p => glob.sync(p, options)
 const flatten = (accumulator, files) => {
-    if (files.length) {
-        accumulator.push(...files)
-    }
-    return accumulator
+	if (files.length) {
+		accumulator.push(...files)
+	}
+	return accumulator
 }
 
 const included = argv._.filter(p => p[0] !== '!').map(get).reduce(flatten, [])
 const excluded = argv._.filter(p => p[0] === '!').map(p => p.slice(1)).map(get).reduce(flatten, [])
 
 included
-    .filter(file => !excluded.includes(file))
-    .forEach(file => process.stdout.write(file + '\n'))
+	.filter(file => !excluded.includes(file))
+	.forEach(file => process.stdout.write(file + '\n'))
